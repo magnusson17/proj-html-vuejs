0a8be14125cf77c03f5a52890f5836ec9f2baf64
Ga <gabri.magni.97@gmail.com>
2022-04-28 12:34:06 +0200

    main comp, section one draft

---
+++ src/components/MainComp.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="main_comp_1st_div">
+        <SectionOne/>
+    </div>
+</template>
+
+<script>
+import SectionOne from './subFolder/subMainFolder/SectionOne.vue';
+
+export default {
+    name: 'MainComp',
+    components: {
+        SectionOne,
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import './cssComp/mixin.scss';
+@import './cssComp/style.scss';
+
+.main_comp_1st_div {
+    @include displayFlexBasic(flex, row, center, center)
+}
+</style>
