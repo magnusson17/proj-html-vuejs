d0da918e6fdd4bb8210d191eabf863e6d4c054cf
Ga <gabri.magni.97@gmail.com>
2022-04-28 16:49:42 +0200

    FooterComp draft

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="h_70vh bg_black">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FooterComp',
+    components: {
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../components/cssComp/mixin.scss';
+@import '../components/cssComp/style.scss';
+</style>
