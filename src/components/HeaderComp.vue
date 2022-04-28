34aa0c324f4f222ee86ab8192b674514b077eb52
Ga <gabri.magni.97@gmail.com>
2022-04-28 11:45:28 +0200

    nav bar draft

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <NavBar/>
+    </div>
+</template>
+
+<script>
+import NavBar from './subFolder/NavBar.vue';
+
+export default {
+    name: 'HeaderComp',
+    components: {
+        NavBar,
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/subFolder/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="nav_bar_1st_div">
+        <div>
+            <span>
+                Nevermind
+            </span>
+        </div>
+
+        <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style scoped lang="scss">
+@import '../cssComp/mixin.scss';
+
+.nav_bar_1st_div {
+    @include displayFlexBasic(flex, row, space-between, center);
+    div {
+        ul {
+            @include displayFlexBasic(flex, row, space-between, center);
+            list-style: none;
+        }
+    }
+}
+</style>
