e3faf64722092d4c2516d98cb7ece17325b85fc2
Ga <gabri.magni.97@gmail.com>
2022-04-28 15:08:43 +0200

    section cc draft

---
+++ src/components/subFolder/subMainFolder/SectionThree.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="section_one_1st_div">
+        <div class="w_50">
+            <PartOne/>
+        </div>
+
+        <div>
+            <PartTwo/>
+        </div>
+    </div>
+</template>
+
+<script>
+import PartOne from './subSectionOne/PartOne.vue';
+import PartTwo from './subSectionOne/PartTwo.vue';
+
+export default {
+    name: 'SectionCc',
+    components: {
+        PartOne,
+        PartTwo,
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../../cssComp/mixin.scss';
+@import '../../cssComp/style.scss';
+
+.section_one_1st_div {
+    @include displayFlexBasic(flex, column, center, center);
+}
+</style>
