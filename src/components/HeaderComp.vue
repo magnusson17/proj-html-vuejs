4547747383b502b6c8cf33380102537c00ce4dd8
Ga <gabri.magni.97@gmail.com>
2022-04-28 11:55:36 +0200

    header comp draft, add img

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
+    <div class="header_comp_1st_div">
         <NavBar/>
+        <div>
+            <h2>
+                prova
+            </h2>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore praesentium deserunt architecto harum dolore suscipit, cumque, ab veniam ducimus sapiente magni expedita consectetur quis facilis perspiciatis quibusdam at doloribus. Excepturi.
+            </p>
+            <button>
+                click
+            </button>
+        </div>
     </div>
 </template>
 
@@ -16,5 +27,10 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import './cssComp/mixin.scss';
 
+.header_comp_1st_div {
+    @include backgroundBasic(no-repeat, cover, center);
+    background-image: url();
+}
 </style>
